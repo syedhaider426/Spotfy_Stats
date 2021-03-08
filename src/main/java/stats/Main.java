daf9ffc499a4ccb9bcf0f1da79492c98e055758e
Syed Haider <shayder426@gmail.com>
2021-03-07 23:22:45 -0800

    This should remove whitelabel error message

--- src/main/java/stats/Main.java
+++ src/main/java/stats/Main.java
@@ -3,12 +3,14 @@ package stats;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Entry point for Spring Boot Application. This application can return song stats for a specific artist.
  * Users can also add an artist to the database.
  */
 @SpringBootApplication
+@ComponentScan(basePackages={"stats"})
 public class Main {
 
     public static void main(String[] args) {
